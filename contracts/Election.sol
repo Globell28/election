d4fdc4a2dd49a63b1d338dd08b030b46c5cd7b2e
GloryPc <glory.joeibekwe@usm.edu>
2019-11-09 13:58:22 -0600

    changes to client side and candidate 2 voting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -6,7 +6,9 @@ contract Election {
         string name;
         uint voteCount;
     }
-
+     
+     //Store accounts that have voted
+     mapping (address => bool) public voters;
     // Read/write candidates
     mapping(uint => Candidate) public candidates;
 
@@ -21,6 +23,18 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+         require(!voters[msg.sender]);
+         
+         require(_candidateId > 0 && _candidateId <= candidatesCount);
+        //Record that voter has voted
+         voters[msg.sender] = true;
+
+        //Update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
     }
 
 }
